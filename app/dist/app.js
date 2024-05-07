7ed4ae5a3c0a4ed897bddf4309f5df176a4da8d6
rstar <rstar0823@icloud.com>
2024-05-07 09:01:17 +0900

    セクション6完了

--- app/dist/app.js
+++ app/dist/app.js
@@ -1,5 +1,23 @@
 "use strict";
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
-    console.log('Clicked!');
-});
+class Department {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.employees = [];
+    }
+    describe() {
+        console.log(`Department (${this.id}): ${this.name}`);
+    }
+    addEmployees(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+const accounting = new Department("d1", "Accounting");
+accounting.addEmployees("Max");
+accounting.addEmployees("Manu");
+accounting.describe();
+accounting.printEmployeeInformation();
